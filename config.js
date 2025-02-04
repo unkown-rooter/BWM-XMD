104c96db079ca025c54f0de4932dda0a6ec06701
unkown-rooter <unknownrooter7@gmail.com>
2025-02-04 11:17:14 +0300

    Update config.js

--- config.js
+++ config.js
@@ -34,7 +34,7 @@ module.exports = { session: process.env.SESSION_ID || '',
                   AUTO_SAVE_CONTACTS : process.env.AUTO_SAVE_CONTACTS || 'yes',
                   AUTO_REJECT_CALL : process.env.AUTO_REJECT_CALL || 'yes',
                   AUTO_BIO : process.env.AUTO_BIO || 'yes',
-                  AUDIO_REPLY : process.env.AUDIO_REPLY || 'yes',
+                  AUDIO_REPLY : process.env.THANK YOU FOR CONTACTING JAPANESEMONK, JAPANESEMONK IS BUSY NOW LET ME KNOW HOW MAY I ASSIST YOU|| 'yes',
     DATABASE_URL,
     DATABASE: DATABASE_URL === databasePath
         ? "postgresql://postgres:bKlIqoOUWFIHOAhKxRWQtGfKfhGKgmRX@viaduct.proxy.rlwy.net:47738/railway" : "postgresql://postgres:bKlIqoOUWFIHOAhKxRWQtGfKfhGKgmRX@viaduct.proxy.rlwy.net:47738/railway",
